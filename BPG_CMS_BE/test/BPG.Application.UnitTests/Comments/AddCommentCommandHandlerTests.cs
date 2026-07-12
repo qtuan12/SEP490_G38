826c5e4615646bd78ae4f3324d3ca5ed279fe4e8
hoangvanduc2k4 <hoangduc060704@gmail.com>
2026-07-12 20:39:23 +0700

    delete 1 test case in Create Comment

--- BPG_CMS_BE/test/BPG.Application.UnitTests/Comments/AddCommentCommandHandlerTests.cs
+++ BPG_CMS_BE/test/BPG.Application.UnitTests/Comments/AddCommentCommandHandlerTests.cs
@@ -1,6 +1,5 @@
 using AutoMapper;
 using BPG.Application.Common.Mappings;
-using BPG.Application.DTOs.DailyLogs;
 using BPG.Application.Features.Comments.Commands;
 using BPG.Application.Features.Comments.Handlers;
 using BPG.Application.IRepositories;
@@ -10,14 +9,7 @@ using BPG.Domain.Entities;
 using BPG.Domain.Exceptions;
 using FluentAssertions;
 using MockQueryable;
-using MockQueryable.Moq;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace BPG.Application.UnitTests.Comments
 {
@@ -82,61 +74,7 @@ namespace BPG.Application.UnitTests.Comments
         }
 
         [Fact]
-        public async Task UTCID01_Handle_AdminUser_ShouldAddCommentSuccessfully()
-        {
-            // Arrange
-            SetupCurrentUser(userId: 10, role: BPG.Domain.Constants.UserRole.Admin);
-
-            var dailyLog = new DailyLog
-            {
-                LogId = 100,
-                CreatedBy = 20,
-                Task = new ProjectTask
-                {
-                    Name = "Building foundation",
-                    Phase = new Phase { ProjectId = 5 }
-                }
-            };
-            _mockDailyLogRepo.Setup(r => r.Query()).Returns(new List<DailyLog> { dailyLog }.AsQueryable().BuildMock());
-
-            var authorUser = new User
-            {
-                UserId = 10,
-                FullName = "Admin User",
-                UserRoles = new List<BPG.Domain.Entities.UserRole>
-                {
-                    new BPG.Domain.Entities.UserRole { Role = new Role { RoleName = BPG.Domain.Constants.UserRole.Admin } }
-                }
-            };
-            _mockUserRepo.Setup(r => r.Query()).Returns(new List<User> { authorUser }.AsQueryable().BuildMock());
-            _mockCommentRepo.Setup(r => r.Query()).Returns(new List<Comment>().AsQueryable().BuildMock());
-
-            var command = new AddCommentCommand { LogId = 100, Content = "Looks good!" };
-
-            // Act
-            var result = await _handler.Handle(command, CancellationToken.None);
-
-            // Assert
-            result.Should().NotBeNull();
-            result.Content.Should().Be("Looks good!");
-            result.AuthorName.Should().Be("Admin User");
-            result.AuthorRole.Should().Be(BPG.Domain.Constants.UserRole.Admin);
-
-            _mockCommentRepo.Verify(r => r.AddAsync(It.Is<Comment>(c => c.Content == "Looks good!"), It.IsAny<CancellationToken>()), Times.Once);
-            _mockUow.Verify(u => u.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
-            _mockNotificationService.Verify(n => n.SendNotificationAsync(
-                20,
-                "Bình luận mới dưới nhật ký",
-                It.Is<string>(s => s.Contains("Admin User") && s.Contains("Building foundation")),
-                NotificationType.Progress,
-                NotificationReferenceType.Task,
-                dailyLog.TaskId,
-                It.IsAny<CancellationToken>()
-            ), Times.Once);
-        }
-
-        [Fact]
-        public async Task UTCID02_Handle_TechnicalManagerUser_ShouldAddCommentSuccessfully()
+        public async Task UTCID01_Handle_TechnicalManagerUser_ShouldAddCommentSuccessfully()
         {
             // Arrange
             SetupCurrentUser(userId: 11, role: BPG.Domain.Constants.UserRole.TechnicalManager);
@@ -176,7 +114,7 @@ namespace BPG.Application.UnitTests.Comments
         }
 
         [Fact]
-        public async Task UTCID03_Handle_ProjectMemberUser_ShouldAddCommentSuccessfully()
+        public async Task UTCID02_Handle_ProjectMemberUser_ShouldAddCommentSuccessfully()
         {
             // Arrange
             SetupCurrentUser(userId: 12, role: BPG.Domain.Constants.UserRole.SiteEngineer); // Normal user
@@ -220,7 +158,7 @@ namespace BPG.Application.UnitTests.Comments
         }
 
         [Fact]
-        public async Task UTCID04_Handle_DailyLogNotFound_ShouldThrowNotFoundException()
+        public async Task UTCID03_Handle_DailyLogNotFound_ShouldThrowNotFoundException()
         {
             // Arrange
             SetupCurrentUser(userId: 10, role: BPG.Domain.Constants.UserRole.Admin);
@@ -237,7 +175,7 @@ namespace BPG.Application.UnitTests.Comments
         }
 
         [Fact]
-        public async Task UTCID05_Handle_NotMemberOrAdminTM_ShouldThrowForbiddenException()
+        public async Task UTCID04_Handle_NotMemberOrAdminTM_ShouldThrowForbiddenException()
         {
             // Arrange
             SetupCurrentUser(userId: 12, role: BPG.Domain.Constants.UserRole.SiteEngineer); // Normal user
@@ -268,7 +206,7 @@ namespace BPG.Application.UnitTests.Comments
         }
 
         [Fact]
-        public async Task UTCID06_Handle_ContentBoundaryMaxLength_ShouldAddCommentSuccessfully()
+        public async Task UTCID05_Handle_ContentBoundaryMaxLength_ShouldAddCommentSuccessfully()
         {
             // Arrange
             SetupCurrentUser(userId: 10, role: BPG.Domain.Constants.UserRole.Admin);
@@ -308,7 +246,7 @@ namespace BPG.Application.UnitTests.Comments
         }
 
         [Fact]
-        public async Task UTCID07_Handle_OwnDailyLog_ShouldNotSendNotificationToCreator()
+        public async Task UTCID06_Handle_OwnDailyLog_ShouldNotSendNotificationToCreator()
         {
             // Arrange
             SetupCurrentUser(userId: 20, role: BPG.Domain.Constants.UserRole.SiteEngineer); // Creator of log is current user
@@ -356,7 +294,7 @@ namespace BPG.Application.UnitTests.Comments
         }
 
         [Fact]
-        public async Task UTCID08_Handle_OtherCommentersExist_ShouldSendNotificationToOtherCommenters()
+        public async Task UTCID07_Handle_OtherCommentersExist_ShouldSendNotificationToOtherCommenters()
         {
             // Arrange
             SetupCurrentUser(userId: 10, role: BPG.Domain.Constants.UserRole.Admin);
@@ -429,7 +367,7 @@ namespace BPG.Application.UnitTests.Comments
         }
 
         [Fact]
-        public async Task UTCID09_Handle_DuplicateCommenters_ShouldSendSingleNotificationPerUser()
+        public async Task UTCID08_Handle_DuplicateCommenters_ShouldSendSingleNotificationPerUser()
         {
             // Arrange
             SetupCurrentUser(userId: 10, role: BPG.Domain.Constants.UserRole.Admin);
@@ -506,7 +444,7 @@ namespace BPG.Application.UnitTests.Comments
         }
 
         [Fact]
-        public async Task UTCID10_Handle_UserNotAuthenticated_ShouldThrowUnauthorizedAccessException()
+        public async Task UTCID9_Handle_UserNotAuthenticated_ShouldThrowUnauthorizedAccessException()
         {
             // Arrange
             SetupCurrentUser(userId: 0, role: "", isAuthenticated: false);
